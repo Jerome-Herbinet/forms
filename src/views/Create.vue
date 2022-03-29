ab453e2ed517d0e16c5e4d15ce11ee066e7fcf02
Jonas Rittershofer <jotoeri@users.noreply.github.com>
2022-03-29 20:12:53 +0200

    Move API to v2
    Signed-off-by: Jonas Rittershofer <jotoeri@users.noreply.github.com>

--- src/views/Create.vue
+++ src/views/Create.vue
@@ -279,7 +279,7 @@ export default {
 			this.isLoadingQuestions = true
 
 			try {
-				const response = await axios.post(generateOcsUrl('apps/forms/api/v1.1/question'), {
+				const response = await axios.post(generateOcsUrl('apps/forms/api/v2/question'), {
 					formId: this.form.id,
 					type,
 					text,
@@ -317,7 +317,7 @@ export default {
 			this.isLoadingQuestions = true
 
 			try {
-				await axios.delete(generateOcsUrl('apps/forms/api/v1.1/question/{id}', { id }))
+				await axios.delete(generateOcsUrl('apps/forms/api/v2/question/{id}', { id }))
 				const index = this.form.questions.findIndex(search => search.id === id)
 				this.form.questions.splice(index, 1)
 			} catch (error) {
@@ -336,7 +336,7 @@ export default {
 			const newOrder = this.form.questions.map(question => question.id)
 
 			try {
-				await axios.post(generateOcsUrl('apps/forms/api/v1.1/question/reorder'), {
+				await axios.post(generateOcsUrl('apps/forms/api/v2/question/reorder'), {
 					formId: this.form.id,
 					newOrder,
 				})
